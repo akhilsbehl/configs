c60a910cf8364ad064602d89821366db0b965ae1
akhilsbehl <akhilsbehl@gmail.com>
2020-08-01 11:16:05 -0400

    Add zip & unzip powershell aliases.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 . C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1
 conda activate 'C:\ProgramData\Anaconda3'
 New-Alias -Name less -Value more
+Set-Alias -Name zip -Value Compress-Archive
+Set-Alias -Name unzip -Value Expand-Archive
