d0cfe1b7c87cff6533da73870d17da96762a9d6e
akhilsbehl <akhilsbehl@gmail.com>
2020-08-19 16:16:17 -0400

    Add alias for ipython with vi keybindings under powershell.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,8 @@ conda activate 'C:\ProgramData\Anaconda3'
 New-Alias -Name less -Value more
 Set-Alias -Name zip -Value Compress-Archive
 Set-Alias -Name unzip -Value Expand-Archive
+
+function Ipython-Vim () {
+  ipython.exe --TerminalInteractiveShell.editing_mode=vi
+}
+New-Alias -Name ipy -Value Ipython-Vim
