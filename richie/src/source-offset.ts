7db31edca3d8caa2c6c1fc169803d29cba81ba53
akhilsbehl <akhilsbehl@gmail.com>
2026-08-22 15:32:18 +0100

    refactor: make Richie a submodule

--- richie/src/source-offset.ts
+++
@@ -1,9 +0,0 @@
-export type RenderedTextPart = { text: string; isReplacementPreview: boolean };
-
-export function sourceTextLength(parts: readonly RenderedTextPart[]): number {
-  return parts.filter((part) => !part.isReplacementPreview).reduce((length, part) => length + part.text.length, 0);
-}
-
-export function sourceText(parts: readonly RenderedTextPart[]): string {
-  return parts.filter((part) => !part.isReplacementPreview).map((part) => part.text).join("");
-}
